3479c19af23652cab83d00bd960d181e8d783288
Devashish <Devashish_gupta@epam.com>
2019-10-16 14:44:25 +0530

    comments added

--- HomeTask2/index.js
+++ HomeTask2/index.js
@@ -1,12 +1,9 @@
 var set = new Set();
-// localStorage.removeItem('flag');
-// localStorage.removeItem('Subscriptions');
-console.log(JSON.stringify(localStorage.getItem('Subscriptions')));
 
+//local Storage Initialization
 if(localStorage.getItem('flag')!=0){
-    var on =[];
-    var ob = JSON.stringify(on);
-    console.log(ob);
+    var array1 =[];
+    var ob = JSON.stringify(array1);
     localStorage.setItem('flag',0);
     localStorage.setItem('Subscriptions',ob);
 }
@@ -25,14 +22,14 @@ head.innerHTML = "<b>Newsfeed</b> <I> &emsp; <small>Yet another newsfeed</small>
 headr.appendChild(head);
 
 
-//Read More Box
+//Read More Box Modal
 let box=document.createElement("div");
 box.className = "Modal";
 box.style.display = "none";
 let conTent =document.createElement("div");
 conTent.className = "modal-content";
 box.appendChild(conTent);
-
+document.body.insertBefore(box, document.body.firstChild);
 
 //Div Main
 var data;
@@ -40,45 +37,31 @@ let main = document.getElementById("main");
 main.className= "article-container";
 main.style.border='none';
 
-//List of Stories
+//Stories Section
 let story = document.createElement("div");
 story.className= "seventy";
-document.body.insertBefore(box, document.body.firstChild);
-
-let form = document.createElement("form");
-
 
 //HTTP Request
 var request = new XMLHttpRequest();
 request.open('GET','https://devashishg.github.io/HomeTasks/HomeTask2/file.json');
-
 request.onload = ()=>{
     data = JSON.parse(request.responseText);
     loadElement(data);
-
-
-    //Button event listener using classname
-    /*var btns = document.querySelectorAll('.button1');
-    for (j = 0; j < btns.length; j++) { 
-    var id = btns[j].getAttribute('id');
-    btns[j].addEventListener('click', ()=> {
-        console.log('Triggered');
-        box.style.display = "block";   
-        conTent.innerHTML = `<h1>${data.array[Number(id)].title}</h1><br/>${data.array[Number(id)].Description}`;
-    });}*/
-
-
 };
 request.send();
 
 
+//Load Complete Page 
 var loadElement = (data)=>{
 
-    //Loop to iterate JSOn
+    //Loop to iterate JSOn data
     for(i=0;i<data.array.length;i++){
-
+        
+        //set for select options
         set.add(data.array[i].Category);
-        //Story Element Creation
+
+
+        //Story Elements e.g. title, description etc 
         let titleText = document.createElement("h1");
         let timeCatText = document.createElement("small");
         let Descr = document.createElement("p");
@@ -87,16 +70,14 @@ var loadElement = (data)=>{
         readMore.innerHTML="Continue Reading";
         readMore.id= `${i}`;
         readMore.setAttribute('onclick',`load('${i}')`);
-        //readMore.setAttribute('onclick',`load('${data.array[i]}')`)
         
 
-        //Json Data Filling var set4 = new Set();
+        //Json Data Filling 
         titleText.innerHTML = `${data.array[i].title}`;
         Descr.innerHTML = `${data.array[i].Description.substring(0,300)}....`;
         timeCatText.innerHTML = `Posted on ${data.array[i].Date} //  Category:${data.array[i].Category}`;
 
         //image 
-
         let storywall = document.createElement("img"); 
         storywall.className= "rest";
         storywall.src='../Resources/pallet.png';
@@ -105,11 +86,12 @@ var loadElement = (data)=>{
         storywall.setAttribute('width', '80px');
         storywall.setAttribute('height', 'auto');
 
-        //One complete Story
+        //One complete Story with image
         let oneArticle = document.createElement('div');
         oneArticle.className = 'article-container hundred';
 
-        let contentStory = document.createElement("div");  // content only 
+        // content only 
+        let contentStory = document.createElement("div");  
         contentStory.className="seventy";
         contentStory.appendChild(titleText);
         contentStory.appendChild(timeCatText);
@@ -120,8 +102,6 @@ var loadElement = (data)=>{
         oneArticle.appendChild(storywall);
         oneArticle.appendChild(contentStory);
         
-
-        //story list 
         
         if(i==data.array.length-1){
             oneArticle.style.border='none';
@@ -131,7 +111,7 @@ var loadElement = (data)=>{
 
     main.appendChild(story);
 
-    //Right hand side bar form 
+    ////form section
 
     let form = document.createElement("form");
     form.setAttribute('action','#');
@@ -174,13 +154,11 @@ var loadElement = (data)=>{
     input.required=true;
     inputdiv.appendChild(input);
     let Subscribe =document.createElement('button');
-    //Subscribe.setAttribute("type","submit");
     Subscribe.innerHTML="SUBSCRIBE";
     inputdiv.appendChild(Subscribe);
     
     form.appendChild(inputdiv);
     main.appendChild(form);
-    //console.log(data); 
 }
 
 //footer
@@ -194,14 +172,8 @@ footer.innerHTML = "<center>© Newsfeed 2019</center>";
 foot.appendChild(footer);
 
 
-// footer.style.padding="20px";
-// footer.style.color ="white";
-// footer.style.position ="fixed";
-// footer.style.bottom ='0';
-// foot.style.background="#11254d";
-// foot.style.width="100%";
 
-//Button onclick method using id
+//Button onclick method for Modal data using id 
 var load = (i)=>{
         let obj=data.array[i];
         //console.log(obj[1]);
@@ -210,20 +182,17 @@ var load = (i)=>{
 }
 
 //closing of popup window
-window.onclick = function(event) {
+window.onclick = (event)=> {
     if (event.target == box) {
       box.style.display = "none";
     }
   }
 
-
+//Filter feeds based on Category
 let filter = (obj)=>{
     var str=obj.value;
     var elements = document.querySelectorAll('.hundred');
-    //console.log(elements);
     for (j = 0; j < elements.length; j++) {
-        // console.log(elements[j].childNodes[1].childNodes[1].innerHTML.split(":")[1]);
-        // console.log(str);
         if(elements[j].childNodes[1].childNodes[1].innerHTML.split(':')[1] != str && str!='All'){
             elements[j].style.display = 'none';
         }else{
@@ -236,6 +205,7 @@ let filter = (obj)=>{
 }
 
 
+//Save data in local storage where obj1 is form object
 let save = (obj1)=> {
     console.log(JSON.parse(localStorage.getItem('Subscriptions')));
     var subsArray = JSON.parse(localStorage.getItem('Subscriptions'));
@@ -245,3 +215,19 @@ let save = (obj1)=> {
 }
 
 
+
+
+
+
+
+
+
+    //Button event listener using classname
+    /*var btns = document.querySelectorAll('.button1');
+    for (j = 0; j < btns.length; j++) { 
+    var id = btns[j].getAttribute('id');
+    btns[j].addEventListener('click', ()=> {
+        console.log('Triggered');
+        box.style.display = "block";   
+        conTent.innerHTML = `<h1>${data.array[Number(id)].title}</h1><br/>${data.array[Number(id)].Description}`;
+    });}*/
