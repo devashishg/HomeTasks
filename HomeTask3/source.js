ddfd3a9504686d3622554f45932f7d2ad5999b68
Devashish <Devashish_gupta@epam.com>
2019-10-18 13:19:24 +0530

    correction of file

--- HomeTask3/source.js
+++ HomeTask3/source.js
@@ -1,25 +1,25 @@
-export {Article};
+import { key } from './index'
+export {Source};
 
 class Source {
     constructor(id, name,...res) {
       this.id = id;
       this.name = name;
       this.array=[];
+      this.url = `https://newsapi.org/v1/articles?source=${thid.id}&apiKey=${key}`;
     }
     
     get id(){
-        return this.id;
+      return this.id;
     }
     get name(){
-        return this.name;
+      return this.name;
     }
-    
-    feeds = () =>{
-        let url = `https://newsapi.org/v1/articles?source=${thid.id}&apiKey=827b0ce7adc847d499f5b4ab0d051ebc`;
-        let promise = new Promise(resolve => resolve(fetch('https://newsapi.org/v2/sources?language=en&country=us&apiKey=827b0ce7adc847d499f5b4ab0d051ebc')),
-                        reject =>{console.log(reject);console.log();})
-                        .then(response=>{console.log(response); console.log('In then');})
-                        .catch(error=>{console.log(error); console.log('In catch');});
+    getSourceFeeds = () =>{
+      new Promise(resolve => resolve(fetch(this.url)))
+        .then(response=>{console.log(response.json()
+          .then(data=>{this.array = data.articles;}))})
+        .catch(error=>{console.log(error);});
     }
     
   }
